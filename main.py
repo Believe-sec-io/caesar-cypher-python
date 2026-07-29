4610f3ab82e26175cba55d4fccd9420ba4094bf5
Believe07 <blovebelieve@gmail.com>
2026-07-29 19:02:16 +0200

    Refactor main function for Caesar cipher

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 from cipher import encrypt, decrypt
 
 def main():
-  print("====Chiffre cesar===")
-  choice= input("Need to (c)rypt or (d)crypt ? ")
-  text=input("Entrer you text: ")
-  shift= int(input("Entrer the offset (integer)"))
+    print("==== Caesar Cipher ====")
+    choice = input("Need to (c)rypt or (d)ecrypt? ")
+    text = input("Enter your text: ")
+    shift = int(input("Enter the offset (integer): "))
 
-if choice== "c":
-  result=encrypt(text,shift)
-elif choice== "d":
-  result=decrypt(text,shift)
-else:
-  ptint("invalide choice")
-  return
+    if choice == "c":
+        result = encrypt(text, shift)
+    elif choice == "d":
+        result = decrypt(text, shift)
+    else:
+        print("Invalid choice")
+        return
 
- print=(f"result: {result}")
+    print(f"Result: {result}")
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
